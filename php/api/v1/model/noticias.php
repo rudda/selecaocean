3888c39e50c440cc0f5291c153e1279cba9d63e5
Rudda Beltrão <beltrao.rudah@gmail.com>
2017-04-02 02:16:06 -0300

    add classe noticia

--- php/api/v1/model/noticias.php
+++ php/api/v1/model/noticias.php
@@ -1,2 +1,15 @@
 <?php
 namespace Beltrao\api\v1\model;
+
+    class noticias{
+
+        public $id;
+        public $titulo;
+        public $data;
+        public $autor;
+        public $descricao;
+        public $img;
+           
+
+
+    }
