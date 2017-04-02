1448d84a5aa328e069c785c8cc7dbece98ef5c8f
Rudda Beltrão <beltrao.rudah@gmail.com>
2017-04-01 22:17:07 -0300

    layou index definido

---
+++ js/menu-eventListener.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Rudda Beltrao on 01/04/2017.
+ */
+$(function () {
+    $(".menu-ico").click(function () {
+
+        $(".menu").show('slow');
+
+    });
+
+    $(".btn-closed").click(function () {
+
+        $(".menu").hide('slow');
+
+    });
+
+
+});
