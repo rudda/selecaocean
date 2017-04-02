f8e11ec43f91cef93889e10705377792388e9333
Rudda Beltrão <beltrao.rudah@gmail.com>
2017-04-02 02:23:07 -0300

    refatorando classes conexao e auth - add classe application

---
+++ php/api/v1/application/Application.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by Rudda Beltrao
+ * Date: 02/04/2017
+ * Time: 02:16
+ * Lab312 developer android  & php backend
+ * www.lab312-icetufam.com.br
+ * beltrao.rudah@gmail.com
+ */
+class Application
+{
+
+}
