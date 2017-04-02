50f878251c8d1f401c5ab2d990b1637508904f3d
Rudda Beltrão <beltrao.rudah@gmail.com>
2017-04-02 02:37:23 -0300

    adicionando metodos addNews e GetNews na classe application

--- php/api/v1/application/Application.php
+++ php/api/v1/application/Application.php
@@ -9,6 +9,8 @@ namespace Beltrao\api\v1\application;
  * beltrao.rudah@gmail.com
  */
 use Beltrao\api\v1\model\noticias as ntc;
+use Beltrao\api\v1\database\conexao;
+use PDO;
 
 class Application
 {
@@ -16,10 +18,57 @@ class Application
 
     public function addNews(ntc $noticia){
 
+        $sql = 'insert into noticias(titulo, descricao, img, autor) values(:titulo, :descricao, :img, :autor)';
+        $db = conexao::connect();
 
+        if($db != null && $db!= false){
+
+            $stmt= $db->prepare($sql);
+
+            $stmt->bindParam(':titulo', $noticia->titulo);
+            $stmt->bindParam(':descricao', $noticia->descricao);
+            $stmt->bindParam(':img', $noticia->img);
+            $stmt->bindParam(':autor', $noticia->autor);
+
+            if($stmt->execute()){
+
+                return true;
+
+            }
+
+            return false;
+
+
+
+        }
+
+       return false;
+    }
+
+    public function getNews($limit){
+
+        $sql = 'select * from noticias order by data limit 0, '.$limit;
+        $db = conexao::connect();
+
+        if($db != null && $db!= false){
+
+            $stmt = $db->prepare($sql);
+
+            while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+                $data[] = $result;
+
+            }
+
+            return json_encode($data);
+
+
+        }
 
 
     }
 
+
+    
 
 }
